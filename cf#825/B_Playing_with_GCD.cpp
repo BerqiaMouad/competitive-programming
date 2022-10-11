c5b65403e1c8f8b5ef3458e98057fd12bf668401
beraqiamouad <berqiamouad@gmail.com>
2022-10-11 09:36:50 +0100

    cf#825

---
+++ cf#825/B_Playing_with_GCD.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O2")
+
+typedef long long ll;
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define sz(c) (ll) c.size()
+#define all(c) c.begin(), c.end()
+#define MOD 1000000007
+
+template <typename T>
+ostream &operator<<(ostream &out, vector<T>& v)
+{
+    for (auto it : v)
+        out << it << ' ';
+    return out;
+}
+template <typename T>
+istream &operator>>(istream &in, vector<T>& v)
+{
+    for (auto &it : v)
+        in >> it;
+    return in;
+}
+template <typename A, typename B>
+istream &operator>>(istream &in, vector<pair<A, B>> &v)
+{
+    for (auto &it : v)
+        in >> it.first >> it.second;
+    return in;
+}
+template <typename A, typename B>
+ostream &operator<<(ostream &out, vector<pair<A, B>> &v)
+{
+    for (auto it : v)
+        out << it.first << ' ' << it.second;
+    return out;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll t ; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+        vector<ll> a(n);
+        cin >> a;
+        a.push_back(1);
+        if(n == 1){
+            YES;
+            continue;
+        }
+
+        // 1 1 2 1 3
+        //  1 1 2 1 1
+
+        vector<ll> b;
+        bool ok = true;
+        for(ll i = 0; i < n ; i++){
+            ll temp = a[i] * a[i+1] / __gcd(a[i], a[i+1]);
+            if(b.empty()){
+                b.push_back(temp);
+                continue;
+            }
+            else {
+                ll tp = __gcd(b.back(), a[i]);
+                if(tp < a[i+1]){
+                    NO;
+                    ok = false;
+                    break;
+                }
+            }
+        }
+        if(ok)
+            YES;
+    }
+
+    return 0;
+}
