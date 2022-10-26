70f13972e04ad6da47db24dfdd5438a5b72fcc11
beraqiamouad <berqiamouad@gmail.com>
2022-10-26 15:09:22 +0100

    practice: upsolving problem C1 and C2 div2#829

---
+++ training/D_Factorial_Divisibility.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O2")
+
+typedef long long ll;
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define sz(c) (ll) c.size()
+#define all(c) c.begin(), c.end()
+#define MOD 1000000007
+
+template <typename T>
+ostream &operator<<(ostream &out, vector<T>& v)
+{
+    for (auto it : v)
+        out << it << ' ';
+    return out;
+}
+template <typename T>
+istream &operator>>(istream &in, vector<T>& v)
+{
+    for (auto &it : v)
+        in >> it;
+    return in;
+}
+template <typename A, typename B>
+istream &operator>>(istream &in, vector<pair<A, B>> &v)
+{
+    for (auto &it : v)
+        in >> it.first >> it.second;
+    return in;
+}
+template <typename A, typename B>
+ostream &operator<<(ostream &out, vector<pair<A, B>> &v)
+{
+    for (auto it : v)
+        out << it.first << ' ' << it.second;
+    return out;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+        ll n, x; cin >>n >> x;
+        vector<ll> a(n);
+        cin >> a; 
+        for(ll i = 0; i< n; i++){
+            if(a[i] >= x){
+                a[i] = 0;
+            }
+            else {
+                a[i] = 1;
+            }
+        }
+        ll sum = 0;
+        for(ll i = 0; i <n; i++){
+            sum += a[i];
+            cout << a[i] << ' ';
+        }
+        cout << "\n";
+        cout << sum << "\n";
+        while(x >= 1 && sum > 0){
+            if(sum % x == 0){
+                sum /= x;
+                x--;
+            }
+            else break;
+        }
+        cout << (x == 0 ? "Yes\n": "No\n");
+
+    return 0;
+}
